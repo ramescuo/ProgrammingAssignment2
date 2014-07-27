414fe7e80ca681642ae6c08331c6a86596d77c7c
ramescuo <ramescuo@hotmail.com>
2014-07-27 19:31:42 +0200

    Fix on cacheSolve function

--- cachematrix.R
+++ cachematrix.R
@@ -21,14 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        inv <- x$getinverse()
-        if(!is.null(inv)) {
-                message("getting cached data")
-                return(inv)
-        }
-        data <- inv$get()
-        inv <- solve(data, ...)
-        x$setmean(inv)
-        inv
+          ## Return a matrix that is the inverse of 'x'
+          inv <- x$getinverse()
+          if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+          }
+          data <- x$get()
+          inv <- solve(data, ...)
+          x$setinverse(inv)
+          inv
 }
